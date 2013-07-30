a37f3d2a40c0f0876e0bdd716d511f607934a755
Florent Lamiraux <florent@laas.fr>
2013-07-30 09:27:20 +0200

    Switch to cmake

---
+++ README.md
@@ -0,0 +1,6 @@
+Graphical interface for Humanoid Path Planner
+
+This package implements a few graphical functionnalities for hpp
+users. The default functionnalities are two menu items : one for
+launching a configuration window for hpp problems, and one used to
+start corba server
--- doc/additionalDoc/package.h
+++ doc/additionalDoc/package.h
@@ -3,7 +3,7 @@
 
 \section intro Introduction
 
-This package implements few graphical functionnalities for hpp users. The default functionnalities are two menu items : one for launching a configuration window for hpp problems, and one used to start corba server
+This package implements a few graphical functionnalities for hpp users. The default functionnalities are two menu items : one for launching a configuration window for hpp problems, and one used to start corba server
 
 \defgroup kppInterface_graphic_Roadmap Graphic Roadmap
 
