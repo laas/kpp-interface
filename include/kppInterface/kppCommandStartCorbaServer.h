d5fd775e1c87d4eab98907632467f6732c1b86fb
Florent Lamiraux <florent@laas.fr>
2013-07-30 08:41:29 +0200

    Notify GNU LGPL v3 license.

--- include/kppInterface/kppCommandStartCorbaServer.h
+++ include/kppInterface/kppCommandStartCorbaServer.h
@@ -1,4 +1,21 @@
-// COPYRIGHT KINEOCAM 2007
+//
+// Copyright (c) 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013 CNRS
+// Authors: David Flavigne, Thomas Moulard and Florent Lamiraux
+//
+// This file is part of kpp-interface
+// kpp-interface is free software: you can redistribute it
+// and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version
+// 3 of the License, or (at your option) any later version.
+//
+// kpp-interface is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Lesser Public License for more details.  You should have
+// received a copy of the GNU Lesser General Public License along with
+// kpp-interface  If not, see
+// <http://www.gnu.org/licenses/>.
+
 #ifndef KPP_CORBASERVERCOMMAND_H
 # define KPP_CORBASERVERCOMMAND_H
 
--- include/kppInterface/kwsGraphicRoadmapDelegate.h
+++ include/kppInterface/kwsGraphicRoadmapDelegate.h
@@ -1,9 +1,20 @@
-/*
-  Copyright CNRS-LAAS
-
-  Authors: David Flavigne, Florent Lamiraux
- 
-*/
+//
+// Copyright (c) 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013 CNRS
+// Authors: David Flavigne, Thomas Moulard and Florent Lamiraux
+//
+// This file is part of kpp-interface
+// kpp-interface is free software: you can redistribute it
+// and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version
+// 3 of the License, or (at your option) any later version.
+//
+// kpp-interface is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Lesser Public License for more details.  You should have
+// received a copy of the GNU Lesser General Public License along with
+// kpp-interface  If not, see
+// <http://www.gnu.org/licenses/>.
 
 #ifndef KWS_GRAPHIC_ROADMAP_DELEGATE_H
 #define KWS_GRAPHIC_ROADMAP_DELEGATE_H
